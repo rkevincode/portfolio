5a996fe88d7f4b6132dc04cb3089fa3404591843
rkevincode <rkevincareer@gmail.com>
2025-02-07 21:39:24 -0800

    feat(001): components and page.js

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { assets } from '@/src/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = ({isDarkMode}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+  
+export default Footer
+  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { assets } from '@/src/assets/assets'
+import Image from 'next/image'
+import React, { useEffect, useRef, useState } from 'react'
+
+const Navbar = ({isDarkMode, setIsDarkMode}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+  
+export default Navbar
